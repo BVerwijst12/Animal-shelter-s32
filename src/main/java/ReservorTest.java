f8d966fb1f007da7febf9b7e85d8f807dffbd43b
bram verwijst <bram.verwijst12@gmail.com>
2019-02-12 13:37:12 +0100

    poging tot unit test

---
+++ src/main/java/ReservorTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservorTest {
+    @Test
+    public void jemoederTest(){
+        assertSame("jemoeder", "jemoeder");
+    }
+}
